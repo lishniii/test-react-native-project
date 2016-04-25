58beadeb481365844d4f59ef885b341aa8431c53
“lishnit” <lishnit@hsenidmobile.com>
2016-04-25 17:29:25 +0530

    contact us page done

--- react-native/TestProject/Third.js
+++ react-native/TestProject/Third.js
@@ -15,21 +15,69 @@ import React, {
     Image,
     DrawerLayoutAndroid,
     ScrollView,
+    ListView
 } from 'react-native';
 
 var Modal = require('react-native-modalbox');
 var _navigator;
+var type = ['M1 Store', 'Apple', 'Samsung'];
+var location = ['All regions', 'Central', 'North', 'South', 'West', 'East'];
 
 class Third extends Component {
     constructor(props, context) {
         super(props, context);
+        var typeDataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        var locationDataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
         this.state = {
             isOpen: false,
             isDisabled: false,
             swipeToClose: true,
-            sliderValue: 0.3
+            sliderValue: 0.3,
+            typeDataSource: typeDataSource.cloneWithRows(type),
+            locationDataSource: locationDataSource.cloneWithRows(location),
+            selectedType: type[0],
+            selectedLocation: location[0]
         };
+
+        this._renderRowType = this._renderRowType.bind(this);
+        this._renderRowLocation = this._renderRowLocation.bind(this);
+    }
+
+    changeStore(rowType) {
+        this.setState({
+            selectedType: rowType
+        });
+        this.closeModalChangeType();
+    }
+
+    changeLocation(row) {
+        this.setState({
+            selectedLocation: row
+        });
+        this.closeModalChangeLocation();
+    }
+
+    _renderRowType(rowType) {
+        return (
+            <ScrollView>
+                <TouchableHighlight onPress={this.changeStore.bind(this, rowType)}
+                                    style={styles.TypeModalList}>
+                    <Text style={{ fontSize:14 }}>{rowType}</Text>
+                </TouchableHighlight>
+            </ScrollView>
+        )
+    }
+
+    _renderRowLocation(row) {
+        return (
+            <ScrollView>
+                <TouchableHighlight onPress={this.changeLocation.bind(this, row)}
+                                    style={styles.LocationModalList}>
+                    <Text style={{ fontSize:14 }}>{row}</Text>
+                </TouchableHighlight>
+            </ScrollView>
+        )
     }
 
     openModalChangeType() {
@@ -117,16 +165,18 @@ class Third extends Component {
                     <ScrollView>
                         <View style={styles.typeContainer}><Text style={styles.typeHeadingText}>TYPE OF
                             STORE</Text></View>
-                        <View style={styles.typePlaceholderContainer}><Text style={styles.typePlaceholderText}>M1
-                            Store</Text><TouchableHighlight style={styles.changeTypeButton}
-                                                            onPress={this.openModalChangeType.bind(this)}><Image
+                        <View style={styles.typePlaceholderContainer}><Text
+                            style={styles.typePlaceholderText}>{this.state.selectedType}</Text><TouchableHighlight
+                            style={styles.changeTypeButton}
+                            onPress={this.openModalChangeType.bind(this)}><Image
                             style={{width: 15, height: 15,}}
                             source={require('./resources/icons/arrow.png')}
                             />
                         </TouchableHighlight></View>
                         <View style={styles.locationContainer}><Text
                             style={styles.locationHeadingText}>AREA</Text></View>
-                        <View style={styles.locationPlaceholderContainer}><Text style={styles.locationPlaceholderText}>Central</Text><TouchableHighlight
+                        <View style={styles.locationPlaceholderContainer}><Text
+                            style={styles.locationPlaceholderText}>{this.state.selectedLocation}</Text><TouchableHighlight
                             style={styles.changeLocationButton} onPress={this.openModalChangeLocation.bind(this)}><Image
                             style={{width: 15, height: 15,}}
                             source={require('./resources/icons/arrow.png')}
@@ -136,61 +186,42 @@ class Third extends Component {
                         <View style={styles.resultsContainer}>
                             <Text style={styles.resultsHeadingText}>STORES</Text>
                         </View>
-                        <View style={styles.resultsDetailsContainer}>
-                            <Image
-                                style={styles.locationIcon}
-                                source={require('./resources/icons/location.png')}
-                                />
-                            <Text style={styles.resultsDetailsText}>Causeway Point
-                                1, Woodlands Square
-                                #03-01 Causeway Point
-                                Singapore 738099
-                                Nearest MRT Station:
-                                Woodlands (NS9) </Text></View>
-                        <View style={styles.resultsDetailsContainer}>
-                            <Image
-                                style={styles.locationIcon}
-                                source={require('./resources/icons/location.png')}
-                                />
-                            <Text style={styles.resultsDetailsText}>Causeway Point
-                                1, Woodlands Square
-                                #03-01 Causeway Point
-                                Singapore 738099
-                                Nearest MRT Station:
-                                Woodlands (NS9)</Text></View>
-                        <View style={styles.resultsDetailsContainer}>
-                            <Image
-                                style={styles.locationIcon}
-                                source={require('./resources/icons/location.png')}
-                                />
-                            <Text style={styles.resultsDetailsText}>Causeway Point
-                                1, Woodlands Square
-                                #03-01 Causeway Point
-                                Singapore 738099
-                                Nearest MRT Station:
-                                Woodlands (NS9)</Text></View>
-                        <View style={styles.resultsDetailsContainer}>
-                            <Image
-                                style={styles.locationIcon}
-                                source={require('./resources/icons/location.png')}
-                                />
-                            <Text style={styles.resultsDetailsText}>Causeway Point
-                                1, Woodlands Square
-                                #03-01 Causeway Point
-                                Singapore 738099
-                                Nearest MRT Station:
-                                Woodlands (NS9)</Text></View>
-                        <View style={styles.resultsDetailsContainer}>
-                            <Image
-                                style={styles.locationIcon}
-                                source={require('./resources/icons/location.png')}
-                                />
-                            <Text style={styles.resultsDetailsText}>Causeway Point
-                                1, Woodlands Square
-                                #03-01 Causeway Point
-                                Singapore 738099
-                                Nearest MRT Station:
-                                Woodlands (NS9)</Text></View>
+                        <View style={styles.results}>
+                            <View style={styles.resultsDetailsContainer}>
+                                <Image
+                                    style={styles.locationIcon}
+                                    source={require('./resources/icons/location.png')}
+                                    />
+                                <View style={styles.resultsDetails}>
+                                    <Text style={styles.resultsStoreName}>Causeway Point</Text>
+                                    <Text style={styles.resultsDetailsAddress}>1, Woodlands Square
+                                        #03-01 Causeway Point
+                                        Singapore 738099
+                                        Nearest MRT Station:
+                                        Woodlands (NS9)</Text>
+                                    <Text style={styles.resultsOpeningHoursHeading}>Operating Hours:</Text>
+                                    <Text style={styles.resultsOpeningHours}>Operating Hours:
+                                        10am-7pm (Mon-Fri)
+                                        10am-5pm (Sat)
+                                        Closed on Sun Public Holidays
+                                    </Text></View></View>
+                        </View>
+                        <View style={styles.results}>
+                            <View style={styles.resultsDetailsContainer}>
+                                <Image
+                                    style={styles.locationIcon}
+                                    source={require('./resources/icons/location.png')}
+                                    />
+                                <View style={styles.resultsDetails}>
+                                    <Text style={styles.resultsStoreName}>Paragon </Text>
+                                    <Text style={styles.resultsDetailsAddress}>290 Orchard Road
+                                        #B1-28/29 Paragon
+                                        Singapore 238859</Text>
+                                    <Text style={styles.resultsOpeningHoursHeading}>Operating Hours:</Text>
+                                    <Text style={styles.resultsOpeningHours}>Operating Hours:
+                                        11am-9pm (Daily)
+                                    </Text></View></View>
+                        </View>
                     </ScrollView>
                 </View>
                 <Modal style={styles.openModalChangeType} position={"center"} ref={'modalChangeType'}
@@ -198,9 +229,17 @@ class Third extends Component {
                     <View>
                         <View style={styles.modalHeading}><Text style={styles.modalHeadingText}>Type of
                             Store</Text>
-                            <TouchableHighlight underlayColor='rgba(0, 0, 0, 0.1)' style={styles.modalCloseButton} onPress={this.closeModalChangeType.bind(this)}><Text
+                            <TouchableHighlight underlayColor='rgba(0, 0, 0, 0.1)' style={styles.modalCloseButton}
+                                                onPress={this.closeModalChangeType.bind(this)}><Text
                                 style={styles.modalHeadingText}>x</Text>
-                            </TouchableHighlight></View>
+                            </TouchableHighlight>
+                        </View>
+                        <View>
+                            <ListView
+                                dataSource={this.state.typeDataSource}
+                                renderRow={this._renderRowType}
+                                />
+                        </View>
                     </View>
                 </Modal>
                 <Modal style={styles.openModalChangeLocation} position={"center"} ref={'modalChangeLocation'}
@@ -208,18 +247,25 @@ class Third extends Component {
                     <View>
                         <View style={styles.modalHeading}>
                             <Text style={styles.modalHeadingText}>Location</Text>
-                            <TouchableHighlight underlayColor='rgba(0, 0, 0, 0.1)' style={styles.modalCloseButton} onPress={this.closeModalChangeLocation.bind(this)}><Text
+                            <TouchableHighlight underlayColor='rgba(0, 0, 0, 0.1)' style={styles.modalCloseButton}
+                                                onPress={this.closeModalChangeLocation.bind(this)}><Text
                                 style={styles.modalHeadingText}>x</Text>
                             </TouchableHighlight>
                         </View>
-
+                        <View>
+                            <ListView
+                                dataSource={this.state.locationDataSource}
+                                renderRow={this._renderRowLocation}
+                                />
+                        </View>
                     </View>
                 </Modal>
             </DrawerLayoutAndroid>
 
         );
-
     }
+
+
 }
 
 const styles = StyleSheet.create({
@@ -362,6 +408,13 @@ const styles = StyleSheet.create({
         color: '#A7A8A3',
         fontWeight: 'bold',
     },
+    results: {
+        flexDirection: 'row',
+
+    },
+    resultsDetails: {
+        flexDirection: 'column',
+    },
     resultsDetailsContainer: {
         width: Dimensions.get('window').width,
         backgroundColor: '#fbfbfb',
@@ -370,12 +423,38 @@ const styles = StyleSheet.create({
         borderColor: '#DCDCDA',
         flexDirection: 'row',
     },
-    resultsDetailsText: {
+    resultsStoreName: {
         width: Dimensions.get('window').width / 100 * 85,
         marginLeft: 10,
         marginTop: 15,
-        marginBottom: 15,
         fontSize: 14,
+        textAlign: 'left',
+        color: '#898989',
+        fontWeight: 'bold'
+    },
+    resultsDetailsAddress: {
+        width: Dimensions.get('window').width / 100 * 85,
+        marginLeft: 10,
+        marginTop: 5,
+        fontSize: 12,
+        textAlign: 'left',
+        color: '#898989',
+    },
+    resultsOpeningHoursHeading: {
+        width: Dimensions.get('window').width / 100 * 85,
+        marginLeft: 10,
+        marginTop: 5,
+        fontSize: 12,
+        textAlign: 'left',
+        color: '#898989',
+        fontWeight: 'bold'
+    },
+    resultsOpeningHours: {
+        width: Dimensions.get('window').width / 100 * 85,
+        marginLeft: 10,
+        marginTop: 5,
+        marginBottom: 15,
+        fontSize: 12,
         textAlign: 'left',
         color: '#898989',
     },
@@ -386,7 +465,7 @@ const styles = StyleSheet.create({
     },
     openModalChangeLocation: {
         width: Dimensions.get('window').width / 100 * 70,
-        height: Dimensions.get('window').height / 100 * 50,
+        height: Dimensions.get('window').height / 100 * 47,
         backgroundColor: '#fbfbfb',
     },
     modalHeading: {
@@ -402,28 +481,30 @@ const styles = StyleSheet.create({
         width: Dimensions.get('window').height / 100 * 10,
         height: Dimensions.get('window').height / 100 * 10,
     },
-    triangle: {
-        width: 0,
-        height: 0,
-        backgroundColor: 'transparent',
-        borderStyle: 'solid',
-        borderLeftWidth: 5,
-        borderRightWidth: 5,
-        borderBottomWidth: 10,
-        borderLeftColor: 'transparent',
-        borderRightColor: 'transparent',
-        borderBottomColor: '#f37124',
-        alignSelf: 'center',
-        transform: [
-            {rotate: '180deg'}
-        ]
-    },
     modalHeadingText: {
         marginTop: 20,
         fontSize: 20,
         textAlign: 'center',
         color: '#fff',
         paddingLeft: 50,
+    },
+    TypeModalList: {
+        height: 40,
+        backgroundColor: '#efefef',
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    LocationModalList: {
+        height: 40,
+        backgroundColor: '#efefef',
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
 
